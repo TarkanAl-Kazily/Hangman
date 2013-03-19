2347be9ec364f3944583a651efa213847148190a
S <s>
2013-03-18 20:29:51 -0700

    {"type":"TI", "runs":"0"}

--- Hangman.java
+++ Hangman.java
@@ -11,10 +11,25 @@ import acm.util.*;
 
 import java.awt.*;
 
+private static final int BODY_PARTS = 8;
+
 public class Hangman extends ConsoleProgram {
 
     public void run() {
-		/* You fill this in */
+    	int index = (int) (Math.random() * HangmanLexicon.getWordCount());
+    	String str = HangmanLexicon.getWord(index);
+    	
 	}
-
+    
+    
+    
+    private char readChar(String display) {
+		String input = readLine(display);
+		while (input.length() != 1) {
+			println("Error: Not a character");
+			input = readLine(display);
+		}
+		return input.charAt(0);
+	}
+    
 }
