8765b5fc56486cb67ef4b2cb93e12a1554b54503
S <s>
2013-03-19 14:18:11 -0700

    {"type":"TI", "runs":"0"}

--- Hangman.java
+++ Hangman.java
@@ -119,6 +119,6 @@ public class Hangman extends ConsoleProgram {
     private int wordLength = str.length();
 	private char[] currentStr = new char[wordLength];
 	private String display;
-	private java.util.List<Character> guessed = new ArrayList<Character>();
+	private List<Character> guessed = new ArrayList<Character>();
     
 }
